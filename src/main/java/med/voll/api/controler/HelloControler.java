95aa35cc515ee4dba58eee58169fadefc49d65c3
Lucas Yago <lucasyago63@gmail.com>
2025-08-08 00:08:52 -0300

    first commit

---
+++ src/main/java/med/voll/api/controler/HelloControler.java
@@ -0,0 +1,15 @@
+package med.voll.api.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloControler {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World Spring!";
+    }
+}
